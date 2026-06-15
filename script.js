c756f2371e7873ef9b272a8b5032746508412172
sabujghorai <ghoraisabuj4@gmail.com>
2026-06-15 11:13:10 +0530

    updated the html,css and javascript file

---
+++ script.js
@@ -0,0 +1,6 @@
+const glow = document.querySelector('.cursor-glow');
+
+document.addEventListener('mousemove', (e) => {
+    glow.style.left = e.clientX + 'px';
+    glow.style.top = e.clientY + 'px';
+});
