b75838a4cac5717c7c888136a25962b2308fd133
sabujghorai <ghoraisabuj4@gmail.com>
2026-06-15 11:15:19 +0530

    removed the all codes of html,css and js

--- script.js
+++
@@ -1,6 +0,0 @@
-const glow = document.querySelector('.cursor-glow');
-
-document.addEventListener('mousemove', (e) => {
-    glow.style.left = e.clientX + 'px';
-    glow.style.top = e.clientY + 'px';
-});
