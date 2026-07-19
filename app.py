e323d977455245c4902503866762673da26cdbe3
sabujghorai <ghoraisabuj4@gmail.com>
2026-07-19 23:06:53 +0530

    updated the index.html and app.py file

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def home():
 def icons(filename):
     return send_from_directory("icons", filename)
 
+@app.route("/image/<path:filename>")
+def image(filename):
+    return send_from_directory("image", filename)
+
 if __name__ == "__main__":
     app.run(debug=True)
